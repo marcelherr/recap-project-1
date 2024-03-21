b385f9d5d4a016b0d4a00ac174e6ba0d529b8800
Marcel Herr <mrclhrr@gmail.com>
2024-03-21 17:06:00 +0100

    add feature create new card to project

--- js/form.js
+++ js/form.js
@@ -4,8 +4,11 @@ const remainingQuestionCharacters = document.querySelector(
 const remainingAnswerCharacters = document.querySelector(
   '[data-js="remainingAnswerCharacters"]'
 );
+
 const questionInput = document.querySelector('[data-js="questionInput"]');
 const answerInput = document.querySelector('[data-js="answerInput"]');
+const tagInput = document.querySelector('[data-js="tagInput"]');
+const form = document.querySelector('[data-js="form"]');
 
 questionInput.addEventListener("input", (event) => {
   remainingQuestionCharacters.innerText = calcRemainingCharacters(event);
@@ -18,48 +21,64 @@ function calcRemainingCharacters(event) {
   return (leftCharacters = 150 - event.target.value.length);
 }
 
-// beispiel:
-// const article = document.createElement("article");
-// article.classList.add("post");
-
-// const p = document.createElement("p");
-// p.classList.add("post__content");
-// p.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
-
-// const footer = document.createElement("footer");
-// footer.classList.add("post__footer");
-
-// const span = document.createElement("span");
-// span.classList.add("post__username");
-// span.textContent = "@username";
-
-// const button = document.createElement("button");
-// button.classList.add("post__button");
-// button.setAttribute("daja-js", "like-button");
-// button.textContent = "♥ Like";
-
-// document.body.append(article);
-// article.append(p, footer);
-// footer.append(span, button);
-
-const formData = new FormData(event.target);
-const data = Object.fromEntries(formData);
-
-createNewCard(data.question, data.answer, data.tag);
-formData.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  createNewCard(data.question, data.answer, data.tag);
+  event.target.reset();
 });
 
-function createNewCard() {
-  const section = document.createElement("section");
-  section.classList.add("cards");
+function createNewCard(question, answer, tags) {
+  const sectionQuestCard = document.createElement("section");
+  sectionQuestCard.classList.add("cards");
 
   const buttonBookmark = document.createElement("button");
-  button.classList.add("card__bookmark");
+  buttonBookmark.classList.add("card__bookmark");
+  buttonBookmark.textContent = "🔖";
+  buttonBookmark.setAttribute("data-js", "bookMarkButton");
 
-  const p = document.createElement("p");
-  p.classList.add("question__text");
-  p.textContent = "new question";
-  const button = document;
+  const pQuestText = document.createElement("p");
+  pQuestText.classList.add("question__text");
+  pQuestText.innerText = questionInput;
+
+  const showAnswerButton = document.createElement("button");
+  showAnswerButton.classList.add("answer__button");
+  showAnswerButton.setAttribute("data-js", "showAnswerButton");
+  showAnswerButton.textContent = "Show Answer";
+
+  const hideAnswerButton = document.createElement("button");
+  hideAnswerButton.classList.add("answer__button");
+  // hideAnswerButton.classList.add("button__hide");
+  hideAnswerButton.setAttribute("data-js", "hideAnswerButton");
+  hideAnswerButton.textContent = "Hide Answer";
+
+  const tagList = document.createElement("ul");
+  tagList.classList.add("tags");
+  tagList.setAttribute("role", "list");
+  tagList.setAttribute("data-js", "tagList");
+
+  const sectionAnswerCard = document.createElement("section");
+  sectionAnswerCard.classList.add("answer__cards");
+  // sectionAnswerCard.classList.add("card__answer__hide");
+  sectionAnswerCard.setAttribute("data-js", "answerCard");
+
+  const answerHeadLine = document.createElement("h3");
+  answerHeadLine.textContent = "Answer:";
+
+  const answerP = document.createElement("p");
+  answerP.textContent = answerInput;
+
+  sectionQuestCard.appendChild(buttonBookmark);
+  sectionQuestCard.appendChild(pQuestText);
+  sectionQuestCard.appendChild(showAnswerButton);
+  sectionQuestCard.appendChild(hideAnswerButton);
+  sectionQuestCard.appendChild(tagList);
+
+  sectionAnswerCard.appendChild(answerHeadLine);
+  sectionAnswerCard.appendChild(answerP);
+
+  document.body.appendChild(sectionQuestCard);
+  document.body.appendChild(sectionAnswerCard);
 }
-// die cards weiter nchbauen mit js siehe index.html
